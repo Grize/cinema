afb87bdb6dfedc1ade1fab26b1d0e06bbd111bce
Grize <myheroreadytodie@gmail.com>
2018-01-30 00:34:14 +0300

    Update movies.rb

--- movies.rb
+++ movies.rb
@@ -1,12 +1,12 @@
 def movies(filmName)
-    ARGV.each do |filmName|
-    case filmName
+  ARGV.each do |filmName|
+  case filmName
 when 'Titanic'
-    puts 'Titanic is a good film'
+  puts 'Titanic is a good film'
 when 'Elky'
-    puts 'Elky is a bad film'
+  puts 'Elky is a bad film'
 else
-    puts 'Never heard about ' + filmName
-end
-end
+  puts 'Never heard about ' + filmName
+  end
+  end
 end
