029c9549a61a40b1c8c2a408b8b792904352a2c6
Paul Barnash <paulbarnash@Pauls-MacBook-Pro.local>
2018-02-02 01:03:26 +0300

    some change

--- movies.rb
+++ movies.rb
@@ -1,12 +1,14 @@
-def movies(filmName)
-    ARGV.each do |filmName|
+def movies(filmList)
+    filmList.each do |filmName|
     case filmName
 when 'Titanic'
     puts 'Titanic is a good film'
 when 'Elky'
     puts 'Elky is a bad film'
 else
-    puts 'Never heard about ' + filmName
+    puts "Never heard about #{filmName}"
 end
 end
 end
+
+movies(ARGV)
