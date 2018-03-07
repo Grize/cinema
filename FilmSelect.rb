f77168fee265f06ab79d24bc2ae1165a2bbc7e74
Paul Barnash <paulbarnash@Pauls-MacBook-Pro.local>
2018-03-07 18:02:11 +0300

    Rework

--- FilmSelect.rb
+++ FilmSelect.rb
@@ -31,45 +31,25 @@ def lineToHash(films)
 end
 
 def filmTiming(film)
-  topLongFilm = []
-  a = []
-  film.each do |h|
-    topLongFilm << h.values_at('timing').to_s.delete('[]""min').to_i
+  arrayWithFiveLongestFilms = []
+  arrayWithFiveLongestFilms << film.max_by(5) do |movie|
+    movie.values_at('timing').to_s.delete('[]""min').to_i
   end
-  b = []
-  d = 0
-  b = topLongFilm.max_by(5) {|v| v}
-  5.times do
-    a << film.select do |h|
-      h.value?("#{b[d]} min")
-    end
-    d += 1
-  end
-  a
+  arrayWithFiveLongestFilms
 end
 
 def filmDate(film)
   comedyFilms = []
-  comedyFilms << film.select do |h|
-    h.values_at('genre').any? {|word| word.include? 'Comedy'}
+  comedyFilms << film.select do |movie|
+    movie.values_at('genre').any? {|word| word.include? 'Comedy'}
   end
-  firstComedy = []
-  comedyFilms.each do |h|
-    h.each do |v| 
-      firstComedy << v.values_at('crearedAdd')
-    end
+  arrayWithTenFirstComedy = []
+  comedyFilms.each do |sudenlyArray|
+   emptyArray << sudenlyArray.min_by(10) do |movie|
+    movie.values_at('crearedAdd').to_s.delete('[]""').to_i
+   end
   end
-  dateArray = []
-  dateArray = firstComedy.min_by(10) {|v| v}
-  i = 0
-  emptyArray = []
-  10.times do
-    emptyArray << film.select do |h|
-      h.value?("#{dateArray[i]}".delete('[]""'))
-    end
-    i += 1
-  end
-  emptyArray
+  arrayWithTenFirstComedy
 end
 
 def filmCountry(film)
@@ -81,8 +61,7 @@ def filmCountry(film)
     else
     end
   end
-  result << "Film didn't creat in USA."
-  puts result << needFilms.length
+  result << needFilms.length
 end
 
 def producersFunc (film)
@@ -90,26 +69,24 @@ def producersFunc (film)
   film.each do |h|
    producerList << h.values_at('producer')
   end
-  puts producerList.uniq.sort_by {|s| s.to_s.delete('""[]').scan(/\w+$/)}
+   producerList.uniq.sort_by {|s| s.to_s.delete('""[]').scan(/\w+$/)}
 end
 
 def printFunc(fTime,fDate)
-  topFiveTiming = []
-  topFiveTiming << 'Top 5 longest film!'
-  firstTenComedy = []
-  firstTenComedy << 'First 10 comedy'
+  resultArrayWithFiveLongestFilm = []
+  resultArrayWithComedy = []
   fTime.each do |h|
     h.each do |v|
-      topFiveTiming << "#{v.values_at('name')} (#{v.values_at('crearedAdd')}; #{v.values_at('genre')}) - #{v.values_at('timing')}".delete('""[]')
+      resultArrayWithFiveLongestFilm << "#{v.values_at('name')} (#{v.values_at('crearedAdd')}; #{v.values_at('genre')}) - #{v.values_at('timing')}".delete('""[]')
     end
   end
   fDate.each do |h|
     h.each do |v|
-      firstTenComedy << "#{v.values_at('name')} (#{v.values_at('crearedAdd')}; #{v.values_at('genre')}) - #{v.values_at('timing')}".delete('""[]')
+      resultArrayWithComedy << "#{v.values_at('name')} (#{v.values_at('crearedAdd')}; #{v.values_at('genre')}) - #{v.values_at('timing')}".delete('""[]')
     end
   end
-  puts topFiveTiming
-  puts firstTenComedy
+  resultArrayWithFiveLongestFilm
+  resultArrayWithComedy
 end
 
 films = fileOpen(ARGV.first)
