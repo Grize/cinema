b0d54d96d60b75706d014265d246388a26c4f0fa
Grize <myheroreadytodie@gmail.com>
2018-03-15 04:19:01 +0300

    Rework with OpenStruct

--- movies.rb
+++ movies.rb
@@ -9,7 +9,7 @@ def checkForFile(filmName)
       films << doArrayWithOpenStr(row)
     end
   else 
-    checkForExist(ARGV.first)
+    films.concat(checkForExist(ARGV.first))
   end
   films
 end
@@ -38,11 +38,10 @@ def doArrayWithOpenStr(row)
     film['createdDate'] = anyStringToDate(row[4])
     film['genre'] = row[5]
     film['timing'] = row[6].to_i
-    film['rating'] = row[7]
+    film['rating'] = ratingInStars(row[7].to_f)
     film['producer'] = row[8]
     film['actors'] = row[9]
-    films << OpenStruct.new(film)
-  films
+    OpenStruct.new(film)
 end
 
 def anyStringToDate(row)
@@ -57,9 +56,9 @@ def anyStringToDate(row)
   end
 end
 
-def findFilmsWithMax(hashFilms)
-  hashFilms.select do |movie| 
-    movie.values_at('name').any?{|string| string.include? 'Max'}
+def findFilmsWithMax(films)
+  films.select do |movie| 
+    movie.name.include? 'Max'
   end
 end
 
@@ -70,14 +69,14 @@ end
 
 def printNameAndRating(findFilmsWithMax)
   findFilmsWithMax.each do |film|
-    "#{film.values_at('name').to_s.delete('[]""')}: #{film.values_at('rating').to_s.delete('""[]')}"
+    "#{film.name}: #{film.rating}"
   end
 end
 
 def filmTiming(film)
   arrayWithFiveLongestFilms = []
   arrayWithFiveLongestFilms << film.max_by(5) do |movie|
-    movie.values_at('timing').to_s.delete('[]""min').to_i
+    movie.timing
   end
   arrayWithFiveLongestFilms
 end
@@ -85,12 +84,12 @@ end
 def filmDate(film)
   comedyFilms = []
   comedyFilms << film.select do |movie|
-    movie.values_at('genre').any? {|word| word.include? 'Comedy'}
+    movie.genre.include? 'Comedy'
   end
   arrayWithTenFirstComedy = []
   comedyFilms.each do |sudenlyArray|
    arrayWithTenFirstComedy << sudenlyArray.min_by(10) do |film|
-    film.values_at('createdDate')
+    film.createdDate
    end
   end
   arrayWithTenFirstComedy
@@ -99,7 +98,7 @@ end
 def filmCountry(film)
   needFilms = []
   film.each do |movie|
-    if movie.value?("USA") == false
+    if movie.country.include?('USA') == false
       needFilms << movie
     else
     end
@@ -110,9 +109,9 @@ end
 def producersList(film)
   producerList = []
   film.each do |movie|
-   producerList << movie.values_at('producer')
+   producerList << movie.producer
   end
-  producerList.uniq.sort_by {|s| s.to_s.delete('""[]').scan(/\w+$/)}
+  producerList.uniq.sort_by {|s| s.scan(/\w+$/)}
 end
 
 def printAllInfoAboutFilm(filmTime,filmDate)
@@ -120,21 +119,21 @@ def printAllInfoAboutFilm(filmTime,filmDate)
   resultArrayWithComedy = []
   filmTime.each do |movie|
     movie.each do |v|
-      resultArrayWithFiveLongestFilm << "#{v.values_at('name')} (#{v.values_at('createdDate')}; #{v.values_at('genre')}) - #{v.values_at('timing')}".delete('""[]')
+      resultArrayWithFiveLongestFilm << "#{v.name} (#{v.createdDate}; #{v.genre}) - #{v.timing} min"
     end
   end
   filmDate.each do |movie|
     movie.each do |v|
-      resultArrayWithComedy << "#{v.values_at('name')} (#{v.values_at('createdDate')}; #{v.values_at('genre')}) - #{v.values_at('timing')}".delete('""[]')
+      resultArrayWithComedy << "#{v.name} (#{v.createdDate}; #{v.genre}) - #{v.timing} min"
     end
   end
-  resultArrayWithFiveLongestFilm
-  resultArrayWithComedy
+   resultArrayWithFiveLongestFilm
+   resultArrayWithComedy
 end
 
 films = checkForFile(ARGV.first)
-filmsWithMax = findFilmsWithMax(hashFilms)
+filmsWithMax = findFilmsWithMax(films)
 printNameAndRating(filmsWithMax)
-filmCountry(hashFilms)
-producersList(hashFilms)
-printAllInfoAboutFilm(filmTiming(hashFilms), filmDate(hashFilms))
+filmCountry(films)
+producersList(films)
+printAllInfoAboutFilm(filmTiming(films), filmDate(films))
