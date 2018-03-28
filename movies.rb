7fbb79c5b58a983b4c893935b7ee814bf490f577
Grize <myheroreadytodie@gmail.com>
2018-03-29 00:49:26 +0300

    Some changes

--- movies.rb
+++ movies.rb
@@ -3,45 +3,28 @@ require 'ostruct'
 require 'date'
 
 def checkForFile(filmName)
-  films = []
-  if ARGV.empty?
-    CSV.foreach('movies.txt', {:col_sep => '|'}) do |row|
-      films << doArrayWithOpenStr(row)
+  films = ARGV.first || 'movies.txt' 
+  arrayWithFilms = []
+  CSV.foreach(films, {:col_sep => '|'}) do |row|
+    arrayWithFilms<< doArrayWithOpenStr(row)
     end
-  else 
-    films.concat(checkForExist(ARGV.first))
-  end
-  films
+    arrayWithFilms
 end
 
-def checkForExist(fileName)
-  films = []
-  ARGV.each do |fileName|
-    if File.exist?(fileName)
-      CSV.foreach("#{fileName}", {:col_sep => '|'}) do |row|
-        films << doArrayWithOpenStr(row)
-      end
-    else
-      puts "Warning! #{fileName} don't exist!"
-    end
-  end
-  films
-end
 
 def doArrayWithOpenStr(row)
-  films = []
-  film = Hash.new
-    film["url"] = row[0]
-    film['name'] = row[1]
-    film['year'] = row[2].to_i
-    film['country'] = row[3]
-    film['createdDate'] = anyStringToDate(row[4])
-    film['genre'] = row[5]
-    film['timing'] = row[6].to_i
-    film['rating'] = ratingInStars(row[7].to_f)
-    film['producer'] = row[8]
-    film['actors'] = row[9]
-    OpenStruct.new(film)
+  {
+    url: row[0],
+    name: row[1],
+    year: row[2].to_i,
+    country: row[3],
+    createdDate: anyStringToDate(row[4]),
+    genre: row[5],
+    timing: row[6].to_i,
+    rating: ratingInStars(row[7].to_f),
+    producer: row[8],
+    actors: row[9]
+  }
 end
 
 def anyStringToDate(row)
@@ -51,40 +34,24 @@ def anyStringToDate(row)
     row += '-01'
     Date.parse(row)
   else
-    row += '-01-01'
+    row += '-01-01'  #change in next time
     Date.parse(row)
   end
 end
 
 def sortByCreatedDate(films)
-  allMonth = []
-  sumOfMonth = Hash.new(0)
-  films.each do |film| 
-    allMonth << film.createdDate.mon
+  allMonths = []
+  sumOfMonths = Hash.new(0)
+  films.each do |film|
+    month = Date::MONTHNAMES[film.createdDate.mon]
+    sumOfMonths[month] += 1
   end
-  allMonth.sort_by{|mon| mon}.each {|mon| sumOfMonth[mon] += 1}
-  puts changeNameofKey(sumOfMonth).to_s.delete('{}:')
-end
-
-def changeNameofKey(sumOfMonth)
-  sumOfMonth[:January] = sumOfMonth.delete(1)
-  sumOfMonth[:February] = sumOfMonth.delete(2)
-  sumOfMonth[:March] = sumOfMonth.delete(3)
-  sumOfMonth[:April] = sumOfMonth.delete(4)
-  sumOfMonth[:May] = sumOfMonth.delete(5)
-  sumOfMonth[:June] = sumOfMonth.delete(6)
-  sumOfMonth[:July] = sumOfMonth.delete(7)
-  sumOfMonth[:August] = sumOfMonth.delete(8)
-  sumOfMonth[:September] = sumOfMonth.delete(9)
-  sumOfMonth[:October] = sumOfMonth.delete(10)
-  sumOfMonth[:November] = sumOfMonth.delete(11)
-  sumOfMonth[:December] = sumOfMonth.delete(12)
-  sumOfMonth
+  puts sumOfMonths
 end
 
 def findFilmsWithMax(films)
   films.select do |movie| 
-    movie.name.include? 'Max'
+    movie[:name].include? 'Max'
   end
 end
 
@@ -95,72 +62,61 @@ end
 
 def printNameAndRating(findFilmsWithMax)
   findFilmsWithMax.each do |film|
-    "#{film.name}: #{film.rating}"
+    puts "#{film[:name]}: #{film[:rating]}"
   end
 end
 
-def filmTiming(film)
+def filmTiming(films)
   arrayWithFiveLongestFilms = []
-  arrayWithFiveLongestFilms << film.max_by(5) do |movie|
-    movie.timing
+  arrayWithFiveLongestFilms << films.max_by(5) do |movie|
+    movie[:timing]
   end
-  arrayWithFiveLongestFilms
+  printAllInfoAboutFilm(arrayWithFiveLongestFilms)
 end
 
-def filmDate(film)
+def filmDate(films)
   comedyFilms = []
-  comedyFilms << film.select do |movie|
-    movie.genre.include? 'Comedy'
+  comedyFilms << films.select do |movie|
+    movie[:genre].include? 'Comedy'
   end
   arrayWithTenFirstComedy = []
   comedyFilms.each do |sudenlyArray|
    arrayWithTenFirstComedy << sudenlyArray.min_by(10) do |film|
-    film.createdDate
+    film[:createdDate]
    end
   end
-  arrayWithTenFirstComedy
+  printAllInfoAboutFilm(arrayWithTenFirstComedy)
 end
 
-def filmCountry(film)
+def filmCountry(films)
   needFilms = []
-  film.each do |movie|
-    if movie.country.include?('USA') == false
+  films.each do |movie|
+    if movie[:country].include?('USA') == false
       needFilms << movie
-    else
     end
   end
   needFilms.length
 end
 
-def producersList(film)
-  producerList = []
-  film.each do |movie|
-   producerList << movie.producer
-  end
-  producerList.uniq.sort_by {|s| s.scan(/\w+$/)}
+def producersList(films)
+  films.map { |film| film[:producer] }
+       .uniq
+       .sort_by { |s| s.scan(/\w+$/) }
 end
 
-def printAllInfoAboutFilm(filmTime,filmDate)
-  resultArrayWithFiveLongestFilm = []
-  resultArrayWithComedy = []
-  filmTime.each do |movie|
-    movie.each do |v|
-      resultArrayWithFiveLongestFilm << "#{v.name} (#{v.createdDate}; #{v.genre}) - #{v.timing} min"
+def printAllInfoAboutFilm(arrayWithNeedFilms)
+  arrayWithNeedFilms.each do |movie|
+    movie.each do |film|
+      "#{film[:name]} (#{film[:createdDate]}; #{film[:genre]}) - #{film[:timing]} min"
     end
   end
-  filmDate.each do |movie|
-    movie.each do |v|
-      resultArrayWithComedy << "#{v.name} (#{v.createdDate}; #{v.genre}) - #{v.timing} min"
-    end
-  end
-   resultArrayWithFiveLongestFilm
-   resultArrayWithComedy
 end
 
 films = checkForFile(ARGV.first)
 filmsWithMax = findFilmsWithMax(films)
-printNameAndRating(filmsWithMax)
-filmCountry(films)
-producersList(films)
-printAllInfoAboutFilm(filmTiming(films), filmDate(films))
-sortByCreatedDate(films)
+#puts printNameAndRating(filmsWithMax)
+#filmCountry(films)
+#filmTiming(films)
+#filmDate(films)
+#producersList(films)
+#sortByCreatedDate(films)
